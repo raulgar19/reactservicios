f0e7d562ff437b6d3864185db068b3272cd6982d
raulgar19 <raulgamu2003@gmail.com>
2025-10-16 20:39:24 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ServicioApiCustomers from "./components/ServicioApiCustomers";
+
+function App() {
+  return <ServicioApiCustomers />;
+}
+
+export default App;
